c2f416d0ddab50b7c4733af1ee4b160ac7bac44c
Poonam <poonamyaduvashi011@gmail.com>
2025-09-24 00:07:47 +0530

    image

--- deploy.bat
+++ deploy.bat
@@ -1,11 +1,11 @@
 @echo off
 REM Stop and remove any existing container named hello-node
-for /F "tokens=*" %%i in ('docker ps -q -f "name=hello-node"') do (
+for /F "tokens=*" %%i in ('docker ps -aq -f "name=hello-node"') do (
     echo Stopping container %%i
     docker stop %%i
     echo Removing container %%i
-    docker rm %%i
+    docker rm -f %%i
 )
 
-REM Run the container in background
-docker run --name hello-node -p 3001:3001 hello-node:latest
+REM Run the container in detached mode
+docker run -d --name hello-node -p 3001:3000 hello-node:latest
